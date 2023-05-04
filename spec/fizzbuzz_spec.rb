324567e3097d355b9cb54a3a97becc9443262568
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-05-04 15:09:02 +0300

    Problem solution for fizzbuzz

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,24 @@
+require './solver'
+
+describe Solver do
+  context 'Testing the fizzbuzz method' do
+    solver = Solver.new
+
+    it 'should return "fizz" for 3' do
+      expect(solver.fizzbuzz(6)).to eql('fizz')
+    end
+
+    it 'should return "buzz" for 5' do
+      expect(solver.fizzbuzz(20)).to eql('buzz')
+    end
+
+    it 'should return "fizzbuzz" for 15' do
+      expect(solver.fizzbuzz(30)).to eql('fizzbuzz')
+    end
+
+    it 'should return "22" for 22' do
+      expect(solver.fizzbuzz(22)).to eql('22')
+    end
+
+  end
+end
